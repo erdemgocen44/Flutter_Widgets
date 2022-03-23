5bb1b38a82768d5cc520a89ff9ebcc9274db932b
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-03-23 22:33:06 +0100

    sorıunsuz çalıştı

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets/widgets/clippers/clip_shadow_path.dart';
+import 'package:widgets/widgets/clippers/custom_clippers.dart';
 
 void main() => runApp(Widgets());
 
@@ -6,15 +8,37 @@ class Widgets extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Widgets',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+        backgroundColor: Colors.red,
+        body: Stack(
+          children: [
+            ClipShadowPath(
+              shadow: const BoxShadow(
+                color: Colors.black,
+                offset: Offset(4, 4),
+                blurRadius: 4,
+                spreadRadius: 8,
+              ),
+              clipper: BigClipper(),
+              child: Container(
+                color: Colors.orangeAccent,
+              ),
+            ),
+            ClipShadowPath(
+              shadow: const BoxShadow(
+                color: Colors.black,
+                offset: Offset(4, 4),
+                blurRadius: 4,
+                spreadRadius: 8,
+              ),
+              clipper: SmallClipper(),
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+          ],
         ),
       ),
     );
