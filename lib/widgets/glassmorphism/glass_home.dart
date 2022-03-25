9b15265aeec59c2bf99dd332d82f3f0a9942ab17
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-03-25 21:38:56 +0100

    glassmorphism pubspec e eklendi

---
+++ lib/widgets/glassmorphism/glass_home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class GlassHome extends StatefulWidget {
+  final String title;
+
+  const GlassHome({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<GlassHome> createState() => _GlassHomeState();
+}
+
+class _GlassHomeState extends State<GlassHome> {
+  bool _isBlur = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Stack(
+          children: [
+            Image.network("https://picsum.photos/400/600"),
+          ],
+        ),
+      ),
+    );
+  }
+}
