a0264b37c79f5f0c8ac616540d7a03eec400ad38
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-03-26 22:46:22 +0100

    flexible widget started

---
+++ lib/widgets/flexible/filexible.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlexibleHome extends StatefulWidget {
+  const FlexibleHome({Key? key}) : super(key: key);
+
+  @override
+  State<FlexibleHome> createState() => _FlexibleHomeState();
+}
+
+class _FlexibleHomeState extends State<FlexibleHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
