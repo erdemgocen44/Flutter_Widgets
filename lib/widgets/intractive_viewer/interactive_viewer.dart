337b1ceedbc235e5777f7e1df6b6e6ffcc86ca73
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-03-27 23:19:23 +0200

    Interactive viewer widget için başladım

---
+++ lib/widgets/intractive_viewer/interactive_viewer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InteractiveViewerPage extends StatefulWidget {
+  const InteractiveViewerPage({Key? key}) : super(key: key);
+
+  @override
+  State<InteractiveViewerPage> createState() => _InteractiveViewerPageState();
+}
+
+class _InteractiveViewerPageState extends State<InteractiveViewerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
