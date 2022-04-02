e68a63a42310cd18e2817378ca66a7ebf0d4ec69
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-04-02 18:37:38 +0200

    alert_dialog bitti sorunsuz çalışıyor

--- lib/widgets/alert_dialog/alert_dialog.dart
+++ lib/widgets/alert_dialog/alert_dialog.dart
@@ -13,19 +13,25 @@ class _AlertDialogHomeState extends State<AlertDialogHome> {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          child: const Text("Show Alert Dialog"),
+          child: const Text(
+            "Show Alert Dialog",
+            style: TextStyle(fontSize: 25),
+          ),
           onPressed: () => showDialog(
             context: context,
             builder: (context) => AlertDialog(
               actions: [
                 TextButton(
                   onPressed: () => Navigator.of(context).pop(),
-                  child: const Text("Close"),
+                  child: const Text(
+                    "Close",
+                  ),
                 )
               ],
               title: const Text(
                 "Flutter is Life!!",
                 textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.red, fontSize: 25),
               ),
               elevation: 15,
               contentPadding: const EdgeInsets.all(20),
