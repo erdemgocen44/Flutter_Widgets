91fac6b553e55cfe640190b7ef4b43a5f2989d27
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-04-02 18:29:06 +0200

    alert_dialog widget için başladım

---
+++ lib/widgets/alert_dialog/alert_dialog.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AlertDialogHome extends StatefulWidget {
+  const AlertDialogHome({Key? key}) : super(key: key);
+
+  @override
+  State<AlertDialogHome> createState() => _AlertDialogHomeState();
+}
+
+class _AlertDialogHomeState extends State<AlertDialogHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Show Alert Dialog"),
+          onPressed: () => showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: Text("Close"),
+                      )
+                    ],
+                  )),
+        ),
+      ),
+    );
+  }
+}
