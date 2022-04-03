b4da5d8fb5833171d5e025383891ba6088a83cc3
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-04-03 11:48:51 +0200

    visibility widget eklendi

---
+++ lib/widgets/visibility/visibility.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class VisibilityHome extends StatefulWidget {
+  const VisibilityHome({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<VisibilityHome> createState() => _VisibilityHomeState();
+}
+
+class _VisibilityHomeState extends State<VisibilityHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    );
+  }
+}
