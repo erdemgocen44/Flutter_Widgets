061e092cf2f99055e65063ccfa8eb09f46a94365
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-04-03 12:13:20 +0200

    visibility widget eklendi

--- lib/widgets/visibility/visibility.dart
+++ lib/widgets/visibility/visibility.dart
@@ -8,15 +8,37 @@ class VisibilityHome extends StatefulWidget {
 }
 
 class _VisibilityHomeState extends State<VisibilityHome> {
+  bool _isVisible = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const SizedBox(
+      body: SizedBox(
         width: double.infinity,
-        child: Center(),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(
+                "https://picsum.photos/200/300",
+                height: MediaQuery.of(context).size.height * 0.2,
+              ),
+              Visibility(
+                visible: _isVisible,
+                child: Image.network(
+                  "https://picsum.photos/300/400",
+                  height: MediaQuery.of(context).size.height * 0.2,
+                ),
+              ),
+              Image.network(
+                "https://picsum.photos/200/300",
+                height: MediaQuery.of(context).size.height * 0.2,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
