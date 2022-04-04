8efbeea64aa29e3ef7032c494068cca4db810a8e
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-04-04 23:36:37 +0200

    FittedBox widget starting

---
+++ lib/widgets/fitted_box/fitted_box_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FittedBoxHome extends StatefulWidget {
+  const FittedBoxHome({Key? key}) : super(key: key);
+
+  @override
+  State<FittedBoxHome> createState() => _FittedBoxHomeState();
+}
+
+class _FittedBoxHomeState extends State<FittedBoxHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
