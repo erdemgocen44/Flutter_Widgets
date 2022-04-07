45ee9b795880363fe002551889d45879c3b93f70
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-04-07 22:28:35 +0200

    adaptive  widget starting

---
+++ lib/widgets/adaptive/adaptive_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveHome extends StatefulWidget {
+  const AdaptiveHome({Key? key}) : super(key: key);
+
+  @override
+  State<AdaptiveHome> createState() => _AdaptiveHomeState();
+}
+
+class _AdaptiveHomeState extends State<AdaptiveHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
