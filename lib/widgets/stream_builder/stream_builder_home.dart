f4c800b42c2af3f759201c247a2e64c81e9e305c
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-04-09 14:56:58 +0200

    streambuilder widget starting

---
+++ lib/widgets/stream_builder/stream_builder_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StreamBuilderHome extends StatefulWidget {
+  const StreamBuilderHome({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<StreamBuilderHome> createState() => _StreamBuilderHomeState();
+}
+
+class _StreamBuilderHomeState extends State<StreamBuilderHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
