2d87faacf4a79abb6df021ae46ad64049af53963
erdemgocen44 <erdemgocen44@gmail.com>
2022-05-01 14:18:58 +0200

    date picker starting

---
+++ lib/widgets/date_picker/date_picker_home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DatePickerHome extends StatefulWidget {
+  const DatePickerHome({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<DatePickerHome> createState() => _DatePickerHomeState();
+}
+
+class _DatePickerHomeState extends State<DatePickerHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    );
+  }
+}
