70246c69a84a57cf441b2287b692e4bd48637ab0
erdemgocen44 <erdemgocen44@gmail.com>
2022-05-01 14:21:42 +0200

    date picker starting

--- lib/widgets/date_picker/date_picker_home.dart
+++ lib/widgets/date_picker/date_picker_home.dart
@@ -8,11 +8,22 @@ class DatePickerHome extends StatefulWidget {
 }
 
 class _DatePickerHomeState extends State<DatePickerHome> {
+  DateTime _dateTime = DateTime(2100);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              '${_dateTime.day}-${_dateTime.month}-${_dateTime.year}',
+              style: const TextStyle(fontSize: 40),
+            )
+          ],
+        ),
       ),
     );
   }
