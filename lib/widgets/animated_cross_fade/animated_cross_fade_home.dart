60f602588ab78cf9eb90e6cc8ab8925dd35c8089
erdemgocen44 <erdemgocen44@gmail.com>
2022-05-13 19:46:10 +0200

    animatedCrossFade widget start

---
+++ lib/widgets/animated_cross_fade/animated_cross_fade_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatedCrossFadeHome extends StatefulWidget {
+  const AnimatedCrossFadeHome({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedCrossFadeHome> createState() => _AnimatedCrossFadeHomeState();
+}
+
+class _AnimatedCrossFadeHomeState extends State<AnimatedCrossFadeHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
