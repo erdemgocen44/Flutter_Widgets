f635f68a156d515b9629631f328a39155fff73ed
erdemgocen44 <erdemgocen44@gmail.com>
2022-05-13 20:37:43 +0200

    animatedCrossFade widget start

--- lib/widgets/animated_cross_fade/animated_cross_fade_home.dart
+++ lib/widgets/animated_cross_fade/animated_cross_fade_home.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
 class AnimatedCrossFadeHome extends StatefulWidget {
-  const AnimatedCrossFadeHome({Key? key}) : super(key: key);
+  const AnimatedCrossFadeHome({Key? key, required this.title})
+      : super(key: key);
   final String title;
   @override
   State<AnimatedCrossFadeHome> createState() => _AnimatedCrossFadeHomeState();
 }
 
 class _AnimatedCrossFadeHomeState extends State<AnimatedCrossFadeHome> {
+  bool _saluration = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          TextButton(
+            onPressed: () {
+              setState(() {
+                _saluration = !_saluration;
+              });
+            },
+            child: Text('Switch'),
+          ),
+        ],
+      ),
     );
   }
 }
