0eb1669510bb8950b42c52d5262863e80354d922
erdemgocen44 <erdemgocen44@gmail.com>
2022-05-21 23:31:02 +0200

    stepper starting

---
+++ lib/widgets/stepper/stepper_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StepperHome extends StatefulWidget {
+  const StepperHome({Key? key}) : super(key: key);
+  final String title;
+  @override
+  State<StepperHome> createState() => _StepperHomeState();
+}
+
+class _StepperHomeState extends State<StepperHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
