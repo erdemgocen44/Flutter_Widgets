676ef7e45559b097b607fe6eda1b360b5f35a750
erdemgocen44 <erdemgocen44@gmail.com>
2022-05-24 21:51:38 +0200

    bottom bar starting

---
+++ lib/widgets/bottom_bar/bottom_bar_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomBarHome extends StatefulWidget {
+  const BottomBarHome({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBarHome> createState() => _BottomBarHomeState();
+}
+
+class _BottomBarHomeState extends State<BottomBarHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
