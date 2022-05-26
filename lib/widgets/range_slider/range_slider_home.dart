4e3c8297000fb3a2eb369c864fb830c2316a1c96
erdemgocen44 <erdemgocen44@gmail.com>
2022-05-26 09:56:50 +0200

    range slider starting

--- lib/widgets/range_slider/range_slider_home.dart
+++ lib/widgets/range_slider/range_slider_home.dart
@@ -11,14 +11,25 @@ class _RangeSliderHomeState extends State<RangeSliderHome> {
   RangeValues values = const RangeValues(0.1, 0.5);
   @override
   Widget build(BuildContext context) {
-    RangeLabels labels=RangeLabels(
+    RangeLabels labels = RangeLabels(
       values.start.toString(),
       values.end.toString(),
     );
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-      ),body: Center(child: ,),
+      ),
+      body: Center(
+        child: RangeSlider(
+            values: values,
+            divisions: 8,
+            labels: labels,
+            onChanged: (newValues) {
+              setState(() {
+                values = newValues;
+              });
+            }),
+      ),
     );
   }
 }
