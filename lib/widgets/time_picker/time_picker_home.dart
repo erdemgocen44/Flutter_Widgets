3b49ac1d3b31d245fed748718d1e613aec319e0d
erdemgocen44 <erdemgocen44@gmail.com>
2022-05-27 17:23:24 +0200

    time picker starting

--- lib/widgets/time_picker/time_picker_home.dart
+++ lib/widgets/time_picker/time_picker_home.dart
@@ -8,11 +8,18 @@ class TimePickerHome extends StatefulWidget {
 }
 
 class _TimePickerHomeState extends State<TimePickerHome> {
+  TimeOfDay? time = const TimeOfDay(hour: 24, minute: 12);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          '${time!.hour.toString()}:${time!.minute.toString()}',
+          style: const TextStyle(fontSize: 60),
+        ),
       ),
     );
   }
