77eb22ae130b31098114c642c60df6017e393691
erdemgocen44 <erdemgocen44@gmail.com>
2022-05-29 23:48:43 +0200

    wrap starting

--- lib/widgets/wrap/wrap_home.dart
+++ lib/widgets/wrap/wrap_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets/core/lorem_picture.dart';
 
 class WrapHome extends StatefulWidget {
   const WrapHome({Key? key, required this.title}) : super(key: key);
@@ -10,6 +11,15 @@ class WrapHome extends StatefulWidget {
 class _WrapHomeState extends State<WrapHome> {
   @override
   Widget build(BuildContext context) {
+    Widget _image = Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Image.network(
+        resim200,
+        height: MediaQuery.of(context).size.height * 0.1,
+        width: MediaQuery.of(context).size.width * 0.1,
+        fit: BoxFit.cover,
+      ),
+    );
     return Scaffold();
   }
 }
