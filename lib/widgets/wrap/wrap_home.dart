4e8e55e18db5630f5a8f6a0be9f86329b06de94d
erdemgocen44 <erdemgocen44@gmail.com>
2022-05-29 23:43:33 +0200

    wrap starting

---
+++ lib/widgets/wrap/wrap_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WrapHome extends StatefulWidget {
+  const WrapHome({Key? key}) : super(key: key);
+
+  @override
+  State<WrapHome> createState() => _WrapHomeState();
+}
+
+class _WrapHomeState extends State<WrapHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
