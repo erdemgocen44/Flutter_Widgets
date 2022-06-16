ff12967dfd72f3947979a686135d0e8a96b5691e
erdemgocen44 <erdemgocen44@gmail.com>
2022-06-16 22:52:47 +0200

    expansion tile starting

---
+++ lib/widgets/expansion_tile/expansion_tile_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExpansionTileHome extends StatefulWidget {
+  const ExpansionTileHome({Key? key}) : super(key: key);
+
+  @override
+  State<ExpansionTileHome> createState() => _ExpansionTileHomeState();
+}
+
+class _ExpansionTileHomeState extends State<ExpansionTileHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
